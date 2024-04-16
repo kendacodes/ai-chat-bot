55fea23e80187d5d52114991ac3df985b05e28e6
Kinda Lamandier <kinnda@Kindas-MacBook-Pro.local>
2024-04-16 00:39:46 -0400

    added everyones updates to one PR - styling, icons, etc

--- frontend-ai-chat-bot/src/components/Answer.jsx
+++ frontend-ai-chat-bot/src/components/Answer.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import CopyAllIcon from "@mui/icons-material/CopyAll";
 import { FaPooStorm } from "react-icons/fa";
 import { Stack } from "@mui/material";
 
 const Answer = ({ response }) => {
   const [thumbDownClicked, setThumbDownClicked] = useState(false);
   const handleThumbDownClick = () => {
-    setThumbDownClicked(true);
+    setThumbDownClicked(!thumbDownClicked);
+  };
+  const handleCopyAllClick = (response) => {
+    // Copy the provided response from variousBotResponses array
+    navigator.clipboard.writeText(response);
+    alert(response + " has been copied to clipboard");
   };
 
   return (
@@ -19,6 +25,7 @@ const Answer = ({ response }) => {
             onClick={handleThumbDownClick}
             style={{ color: thumbDownClicked ? "red" : "inherit" }}
           />
+          <CopyAllIcon onClick={() => handleCopyAllClick(response)} />
         </Stack>
       </li>
     </>
